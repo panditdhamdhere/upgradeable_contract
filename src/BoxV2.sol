d5295ada485893dbe06a5b9c3cfdc7a72949c819
panditdhamdhere <dhamdherepandit@gmail.com>
2023-07-17 16:14:52 +0530

    box v2 updated again

--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -3,21 +3,9 @@
 pragma solidity ^0.8.19;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract BoxV2 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+contract BoxV2 is UUPSUpgradeable {
     uint256 internal number;
-
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() {
-        _disableInitializers();
-    }
-
-    function initialize() public initializer {
-        __Ownable_init();
-        __UUPSUpgradeable_init();
-    }
 
     function setNumber(uint256 _number) external {}
 
@@ -29,7 +17,5 @@ contract BoxV2 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
         return 2;
     }
 
-    function _authorizeUpgrade(
-        address newImplementation
-    ) internal override onlyOwner {}
+    function _authorizeUpgrade(address newImplementation) internal override {}
 }
