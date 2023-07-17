0b189e334f1e40010c140dc9fe773388b558d8ba
panditdhamdhere <dhamdherepandit@gmail.com>
2023-07-18 00:25:27 +0530

    now test time

--- script/UpgradeBox.s.sol
+++ script/UpgradeBox.s.sol
@@ -12,5 +12,22 @@ contract UpgradeBox is Script {
     function run() external returns (address) {
         address mostRecentlyDeployedProxy = DevOpsTools
             .get_most_recent_deployment("ERC1967Proxy", block.chainid);
+
+        vm.startBroadcast();
+        BoxV2 newBox = new BoxV2();
+        vm.stopBroadcast();
+        address proxy = upgradeBox(mostRecentlyDeployedProxy, address(newBox));
+        return proxy;
+    }
+
+    function upgradeBox(
+        address proxyAddress,
+        address newBox
+    ) public returns (address) {
+        vm.startBroadcast();
+        BoxV1 proxy = BoxV1(proxyAddress);
+        proxy.upgradeTo(address(newBox)); // proxy contract now points to this new address
+        vm.stopBroadcast();
+        return address(proxy);
     }
 }
