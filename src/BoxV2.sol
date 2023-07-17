c035c1b230f8efc5b3c996471fdde003b645f9e4
panditdhamdhere <dhamdherepandit@gmail.com>
2023-07-18 01:18:53 +0530

    testupgrades test passed

--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -7,7 +7,9 @@ import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/U
 contract BoxV2 is UUPSUpgradeable {
     uint256 internal number;
 
-    function setNumber(uint256 _number) external {}
+    function setNumber(uint256 _number) external {
+        number = _number;
+    }
 
     function getNumber() external view returns (uint256) {
         return number;
